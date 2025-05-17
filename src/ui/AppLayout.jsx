e134cf2885d9543ba6f69df280ca651bf05cdcd5
Arthur <arthur91021@gmail.com>
2025-05-16 17:20:55 -0700

    Using Css Grid

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,25 +1,25 @@
-import Header from "./Header";
-import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "./Loader";
+import Header from './Header'
+import CartOverview from '../features/cart/CartOverview'
+import { Outlet, useNavigation } from 'react-router-dom'
+import Loader from './Loader'
 
 function AppLayout() {
-  const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
-  return (
-    <div className="layout">
-      {isLoading && <Loader />}
+    const navigation = useNavigation()
+    const isLoading = navigation.state === 'loading'
+    return (
+        <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+            {isLoading && <Loader />}
 
-      <Header />
-
-      <main>
-        <h1>Content</h1>
-        <Outlet />
-      </main>
-
-      <CartOverview />
-    </div>
-  );
+            <Header />
+            <div className="overflow-scroll">
+                <main className="mx-auto max-w-3xl">
+                    <h1>Content</h1>
+                    <Outlet />
+                </main>
+            </div>
+            <CartOverview />
+        </div>
+    )
 }
 
-export default AppLayout;
+export default AppLayout
