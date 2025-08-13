2439fd3f43832e9bf15a53ed868a088c163c7071
Arthur <arthur91021@gmail.com>
2025-08-13 07:57:25 -0700

    reading and updating the user state

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom'
 import { createOrder } from '../../services/apiRestaurant'
 import Button from '../../ui/Button'
+import { useSelector } from 'react-redux'
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -40,6 +41,7 @@ const fakeCart = [
 ]
 
 function CreateOrder() {
+    const username = useSelector((state) => state.user.username)
     const navigation = useNavigation()
     const isSubmitting = navigation.state === 'submitting'
 
@@ -62,6 +64,7 @@ function CreateOrder() {
                         className="input grow"
                         type="text"
                         name="customer"
+                        defaultValue={username}
                         required
                     />
                 </div>
